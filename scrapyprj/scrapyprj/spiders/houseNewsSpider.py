2b1f5b3266af00040b1cf855badc835b08022645
xueliang.xl <xueliang.xl@alibaba-inc.com>
2016-05-12 09:28:51 +0800

    just do it

--- scrapyprj/scrapyprj/spiders/houseNewsSpider.py
+++ scrapyprj/scrapyprj/spiders/houseNewsSpider.py
@@ -10,7 +10,7 @@ sys.setdefaultencoding('utf-8')
 class HousenewsspiderSpider(scrapy.Spider):
     
     name = "houseNewsSpider"
-    allowed_domains = ["www.weixinyidu.com"]
+    allowed_domains = ["weixinyidu.com"]
 
    	# start_urls = [
     #    'http://www.weixinyidu.com/a_958',
@@ -33,25 +33,24 @@ class HousenewsspiderSpider(scrapy.Spider):
     def parse_seed(self,response):
     	"""parse seed to generate sublist"""
     	#get news list
-    	urlList = response.xpath("//div[@class='news_content']//li/a").extract()
+    	urlList = response.xpath("//div[@class='news_content']//li/a/@href").extract()
     	for url in urlList:
-    		yield scrapy.Request(url,callback=self.parse_detail)
+    		yield scrapy.Request('http://www.weixinyidu.com'+url,callback=self.parse_detail)
 
     #generate newsItem from detail 
     def parse_detail(self,response):
-    	
     	houseNews = HouseNewsItem()
     	#标题
     	houseNews['title'] = response.xpath("//h1[@class='news_title']/text()").extract()[0]
+    	print houseNews['title']
     	#时间
-    	houseNews['releaseTime'] = response.xpath("//span[@class='news_time']/text()").extract()[0]
+    	#houseNews['releaseTime'] = response.xpath("//span[@class='news_time']/text()").extract()[0]
     	#阅读量
-    	houseNews['readCount'] =response.xpath("//div[@class='left']/span[@class = 'news_read_no'][2]").extract()[0]
+    	#houseNews['readCount'] =response.xpath("//div[@class='left']/span[@class = 'news_read_no'][2]").extract()[0]
     	#点赞量
-    	houseNews['thumbCount'] =response.xpath("//div[@class='right']/span[@class = 'news_read_no'][1]").extract()[0]
+    	#houseNews['thumbCount'] =response.xpath("//div[@class='right']/span[@class = 'news_read_no'][1]").extract()[0]
     	#关键词，热词
-    	houseNews['keywords'] = response.xpath("//a[@class='hot_txt']/text()").extract()
-    	
+    	#houseNews['keywords'] = response.xpath("//a[@class='hot_txt']/text()").extract()
     	yield houseNews
 
 
