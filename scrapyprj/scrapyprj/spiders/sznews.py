bc310856b1695ad09e6cddb6de006a6f42c54c14
Alexoner <onerhao@gmail.com>
2016-05-12 13:08:00 +0800

    sznews: done

--- scrapyprj/scrapyprj/spiders/sznews.py
+++ scrapyprj/scrapyprj/spiders/sznews.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 import time
 import urlparse
 import scrapy
@@ -35,14 +36,39 @@ class SznewsSpider(scrapy.Spider):
                         'uid': 'detail',
                     },
                     'dont_merge_cookies': True,
-                    'cookiejar': i,
+                    'cookiejar': 2 * i + 1,
                 })
+
+        next_page = response.xpath(
+            '//div[@class="fl w660-news-index"]/div/center/span/following-sibling::a[1]/@href')
+        yield scrapy.Request(
+            urlparse.urljoin(response.url,
+                             safe_extract(next_page)),
+            callback=self.parse,
+            headers={},
+            meta={
+                'item': {
+                    'uid': 'list',
+                },
+                'dont_merge_cookies': True,
+                'cookiejar': random.randint(1, 10000),
+            })
         pass
 
     def parse_detail(self, response):
-        # TODO: manybe consider goose or newspaper to deal with article extraction
-        with open('/tmp/a.html', 'w') as f:
-            f.write(response.body)
+        # TODO: manybe consider goose or newspaper to deal with article
+        # extraction
+        #  with open('/tmp/a.html', 'w') as f:
+            #  f.write(response.body)
+        article = extract_article(raw_html=response.body)
+        if response.xpath('//*[@id="source_baidu"]/a'):
+            source = response.xpath('//*[@id="source_baidu"]/a')
+            source_name = safe_extract(source.xpath('./text()'))
+            source_url = safe_extract(source.xpath('./@href'))
+        else:
+            source = response.xpath('//*[@id="source_baidu"]')
+            source_name = str(safe_extract(source.xpath('./text()')).split('：')[1]).strip()
+            source_url = None
         yield HouseNewsItem({
             #  'html_document': [safe_extract(response.xpath('//*[@id="PrintTxt"]'))],
             'url': [response.url],
@@ -51,7 +77,12 @@ class SznewsSpider(scrapy.Spider):
             'release_time': safe_extract(response.xpath('//*[@id="pubtime_baidu"]/text()')),
             'summary': safe_extract(response.xpath('//p[@id="fzy"]/text()')),
             #  'content': safe_extract(response.xpath('(//*[@id="PrintTxt"]/div[2]/p/font|//*[@id="PrintTxt"]/div[2]/p)/text()')),
-            'content': extract_article(raw_html=response.body)['cleaned_text'],
-            'source_name': safe_extract(response.xpath('//*[@id="source_baidu"]/text()')),
+            'content': article['cleaned_text'],
+            'source_name': source_name,
+            'source_url': source_url,
+            'keywords': article['meta']['keywords'],
         })
+
+        # TODO: an article may be separated in two pages, we need to visit the
+        # next page
         pass
--- scrapyprj/scrapyprj/utils/__init__.py
+++ scrapyprj/scrapyprj/utils/__init__.py
@@ -3,10 +3,10 @@ from goose import Goose
 from goose.text import StopWordsChinese
 
 
-def safe_extract(selectors, separator='\t'):
-    if not selectors:
+def safe_extract(selectorList, separator='\t'):
+    if not selectorList:
         return None
-    return separator.join(selectors.extract())
+    return separator.join(selectorList.extract())
 
 def extract_article(raw_html=None):
     return extract_article.goose.extract(raw_html=raw_html).infos
