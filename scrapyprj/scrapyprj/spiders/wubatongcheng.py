2a97b673499af15731d70ff57918a07346012da7
xueliang.xl <xueliang.xl@alibaba-inc.com>
2016-05-22 00:32:45 +0800

    just do it

--- scrapyprj/scrapyprj/spiders/wubatongcheng.py
+++ scrapyprj/scrapyprj/spiders/wubatongcheng.py
@@ -55,7 +55,7 @@ class WubatongchengSpider(scrapy.Spider):
             alias_name = alias_name.replase('(',"")
             alias_name = alias_name.replase(')',"")
             alias_name = alias_name.strip()
-
+	#省市和坐标
         locationInfo = safe_extract(response.xpath("//meta[@name='location']/@content"))
         province = '广东'
         city = '深圳'
@@ -71,7 +71,9 @@ class WubatongchengSpider(scrapy.Spider):
                 if i == 2:
                     longitude = tmpList[2].split('=')[1].split(",")[0]
                     latitude = tmpList[2].split('=').split(",")[1]
-        area = ''
+
+	#区和小区
+	area = ''
         subarea = ''
         areaList = safe_extract(response.xpath("//dl[@class='bhrInfo']/dd[2]//a[contains(@href,'xiaoqu')]/text()"))
         if areaList:
@@ -81,9 +83,12 @@ class WubatongchengSpider(scrapy.Spider):
                 if i == 1:
                     subarea = areaList[1]
                     break
+	# 地址和邮编
         address = safe_extract(response.xpath("//span[@class='ddinfo']/a[contains(@href,'xiaoqu')]/parent::span/text()"))
         postcode = safe_extract(response.xpath("//dd/span[contains(text(),'小区邮编')]/parent::dd/text()"))
-        year_type = safe_extract(response.xpath("//dd/span[contains(text(),'建筑年代')]/parent::dd/text()"))
+
+	#建筑年代和类型
+	year_type = safe_extract(response.xpath("//dd/span[contains(text(),'建筑年代')]/parent::dd/text()"))
         year = ''
         type = ''
         if year_type:
@@ -93,12 +98,14 @@ class WubatongchengSpider(scrapy.Spider):
                 if i == 1:
                     type = year_type[1]
 
+	#物业费
         wuyeMoney = safe_extract(response.xpath("//dd/span[contains(text(),'物业费')]/parent::dd/span[@class='ddinfo']/text()"))
         if wuyeMoney:
             wuyeMoney = wuyeMoney.split("/")[0]
 
+	#开发商、物业公司
         kaifashang = safe_extract(response.xpath("//dd/span[contains(text(),'开发商')]/parent::dd/text()"))
-
+        wuyeCompany = safe_extract(response.xpath("//d/span[contains(text(),'物业公司')]/parent::dd/text()"))
 
 
 
